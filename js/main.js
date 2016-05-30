09f239c9b60d44662e545bbc4ef5a7cbc5315a96
brunorafael8 <bruninho_lito@hotmail.com>
2016-05-30 13:41:31 -0300

    NAVBAR 50%

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $("#slider").owlCarousel({
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
+      singleItem:true,
+      autoPlay: 3000,
+      mouseDrag: false
  
 });
- 
+$(".find-t").hover(function(){
+    $(".find-toggler").show();
+});
