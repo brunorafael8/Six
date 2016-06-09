00a94ceddf3eccd4940e774fd2910e001d91db1f
brunorafael8 <bruninho_lito@hotmail.com>
2016-06-09 15:02:13 -0300

    modal fim; footer 50%

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-$("#slider").owlCarousel({
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true,
-      autoPlay: 3000,
-      mouseDrag: false
- 
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(500);
 });
-$(".find-t").hover(function(){
-    $(".find-toggler").show();
-});
+
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
